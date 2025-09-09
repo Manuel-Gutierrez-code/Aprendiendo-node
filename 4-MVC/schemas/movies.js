e853b573cd5ae9cfc73bc323bcf31dceee42af79
Manuel-Gutierrez-code <gutierrez.manuel.imp@gmail.com>
2025-09-09 01:44:47 -0300

    Aprendiendo arquitectura MVC

---
+++ 4-MVC/schemas/movies.js
@@ -0,0 +1,30 @@
+import z from 'zod'
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Movie title must be a string',
+    require_error: 'Movie title is required'
+  }),
+  year: z.number().int().min(1900).max(2025),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10).default(5).optional(),
+  poster: z.string().url({
+    message: 'Invalid poster URL'
+  }),
+  genre: z.array(
+    z.enum(['Crime', 'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']),
+    {
+      require_error: 'Movie genre is required',
+      invalid_type_error: 'Movie genre must be an array of enum Genre'
+    }
+  )
+})
+
+export function validateMovie (objetc) {
+  return movieSchema.safeParse(objetc)
+}
+
+export function validatePartialMovie (object) {
+  return movieSchema.partial().safeParse(object)
+}
